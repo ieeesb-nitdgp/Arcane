4d1df8a16e730a8261168b127f2d860837c0610b
sa-y-an <sayan.ee.nitd@gmail.com>
2021-10-23 23:02:00 +0530

    correction

--- quiz/views.py
+++ quiz/views.py
@@ -223,6 +223,7 @@ def Passcode(request):
                 player = get_object_or_404(Player, user=request.user)
                 player.level2 = -1
                 player.score += 60
+                player.last_submit = timezone.now()
                 player.save()
                 question_level = player.question_level
                 if ((question_level) - 1 > 0):
