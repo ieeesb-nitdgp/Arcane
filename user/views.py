f2e36474ca376f4653dd09e7c47eca2de43f723d
Sayan Mondal <sayan.ee.nitd@gmail.com>
2021-12-07 21:10:01 +0530

    Domain changed

--- user/views.py
+++ user/views.py
@@ -21,7 +21,7 @@ def logout(request):
     django_logout(request)
     domain = settings.SOCIAL_AUTH_AUTH0_DOMAIN
     client_id = settings.SOCIAL_AUTH_AUTH0_KEY
-    return_to = 'http://arcane.ieeesbnitdgp.com' # this can be current domain
+    return_to = 'https://ieee-arcane.herokuapp.com/' # this can be current domain
     return redirect(f'https://{domain}/v2/logout?client_id={client_id}&returnTo={return_to}')
 
 
