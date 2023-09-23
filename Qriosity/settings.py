30c2e21c73d1278e31c863084900164b35902ed9
Nitin <nk17oct@gmail.com>
2023-09-23 23:12:25 +0530

    add allowed hosts

--- Qriosity/settings.py
+++ Qriosity/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['herokuapp.com', 'arcane.ieeesbnitdgp.com']
+ALLOWED_HOSTS = ['herokuapp.com', 'arcane.ieeesbnitdgp.com', 'onrender.com']
 
 
 # Application definition
