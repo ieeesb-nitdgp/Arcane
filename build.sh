7e96378d59418c313bf0c803fa229e05bf83a764
Sahitya Kumar Choubey <sahityakumarchoubey@gmail.com>
2024-10-05 00:15:52 +0530

    add script for superuser

--- build.sh
+++ build.sh
@@ -2,7 +2,16 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Apply database migrations
 python manage.py migrate
+
+# Create a superuser if environment variables are set
+if [ "$CREATE_SUPERUSER" = "true" ]; then
+    python manage.py createsu
+fi
