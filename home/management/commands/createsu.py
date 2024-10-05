51278a6dba24498f38922835b14e31a83e9904cd
Sahitya Kumar Choubey <sahityakumarchoubey@gmail.com>
2024-10-05 10:02:18 +0530

    fix superuser script

--- home/management/commands/createsu.py
+++ home/management/commands/createsu.py
@@ -1,11 +1,12 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 import os
 
 class Command(BaseCommand):
     help = 'Create a superuser with environment variables'
 
     def handle(self, *args, **kwargs):
+        User = get_user_model()
         username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
         email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
         password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
